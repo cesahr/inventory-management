8453f393f51bda1a86c90ebf9ab538cc1512aeb6
cesahr <145717277+cesahr@users.noreply.github.com>
2024-11-20 20:05:21 -0500

    inventory

---
+++ Backend/ecosystem.config.js
@@ -0,0 +1,13 @@
+modele.exports={
+    apps: [
+        {
+            name: "inventory-management",
+            script: "npm",
+            args: "run dev",
+            env: {
+                NODE_ENV:"develoment",
+                ENV_VAR1:"environmet-variable"
+            },
+        },
+    ],
+};
